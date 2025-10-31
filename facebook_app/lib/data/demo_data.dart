4c45885da4dd86f9b497ff0f9aafff431a38d71a
Eranga <erangakavishanka@icloud.com>
2025-11-01 00:02:43 +0530

    changed demo data file to use in home screen

--- facebook_app/lib/data/demo_data.dart
+++ facebook_app/lib/data/demo_data.dart
@@ -4,12 +4,15 @@ class DemoData {
 
   static List<StoryModel> stories = [
 
-    StoryModel(profilePicture: 'https://th.bing.com/th/id/OIP.DnQq__W5pibltm9H65jDLQHaE8?w=301&h=201&c=7&r=0&o=7&dpr=1.3&pid=1.7&rm=3',
-    story:  'https://th.bing.com/th/id/OIF.RyLQDxxq2TiyvoH3IK3A1w?w=229&h=180&c=7&r=0&o=7&dpr=1.3&pid=1.7&rm=3'
-    ),
+    StoryModel(profilePicture: '', story: ''),
+
+  
 
     StoryModel(profilePicture: 'https://th.bing.com/th/id/OIP.GjQkNLBt1b2rykCqh4paZwHaLI?w=122&h=184&c=7&r=0&o=7&dpr=1.3&pid=1.7&rm=3',
     story:  'https://th.bing.com/th/id/OIF.loeLi75eugN2z2EEKgiSOg?w=225&h=180&c=7&r=0&o=7&dpr=1.3&pid=1.7&rm=3'
+    ),
+      StoryModel(profilePicture: 'https://th.bing.com/th/id/OIP.DnQq__W5pibltm9H65jDLQHaE8?w=301&h=201&c=7&r=0&o=7&dpr=1.3&pid=1.7&rm=3',
+    story:  'https://th.bing.com/th/id/OIF.RyLQDxxq2TiyvoH3IK3A1w?w=229&h=180&c=7&r=0&o=7&dpr=1.3&pid=1.7&rm=3'
     ),
 
     StoryModel(profilePicture:'https://th.bing.com/th/id/OIP.kfM6gE5n_IxPxrdgZg7SigHaLH?w=116&h=183&c=7&r=0&o=7&dpr=1.3&pid=1.7&rm=3' ,
@@ -22,6 +25,8 @@ class DemoData {
 
 
   ];
+
+  static String profilePic = 'https://tse1.mm.bing.net/th/id/OIP.5E1nwnhAzODo8pYnFvKtXgHaHa?rs=1&pid=ImgDetMain&o=7&rm=3';
 
 
 
