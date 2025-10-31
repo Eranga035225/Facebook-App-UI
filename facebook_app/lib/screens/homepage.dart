aa59b8fae521c50d4c3dcaf55708b81cf584a9b1
Eranga <erangakavishanka@icloud.com>
2025-10-31 14:31:51 +0530

    finished making the next row of the hoem screen

--- facebook_app/lib/screens/homepage.dart
+++ facebook_app/lib/screens/homepage.dart
@@ -41,7 +41,30 @@ class HomePage extends StatelessWidget {
                  ),
               
           
-            ],)
+            ],),
+            SizedBox(height: 10,),
+
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              
+              children: [
+              Icon(Icons.home, size: 40,color: Colors.blue.shade600,),
+              Icon(Icons.people_outline, size: 40, color: Colors.grey.shade700),
+              Icon(Icons.ondemand_video_outlined, size: 40, color: Colors.grey.shade700),
+              Icon(Icons.maps_home_work_outlined, size: 40, color: Colors.grey.shade700),
+              Icon(Icons.notifications_none_outlined, size: 40, color: Colors.grey.shade700),
+              Icon(Icons.menu, size: 40, color: Colors.grey.shade700),
+
+
+
+
+
+            ],),
+            Divider(
+              color: Colors.grey.shade500
+            ),
+
             
           
           
