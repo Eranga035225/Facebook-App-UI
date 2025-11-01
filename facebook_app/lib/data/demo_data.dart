b6b5e680088009220ee97cb0e47f10bf913e9b6d
Eranga <erangakavishanka@icloud.com>
2025-11-01 23:41:19 +0530

    developed completely home page of facebook app

--- facebook_app/lib/data/demo_data.dart
+++ facebook_app/lib/data/demo_data.dart
@@ -32,9 +32,9 @@ class DemoData {
 
   static List<PostModel> posts = [
 
-    PostModel(name: 'John Doe', profilePicture: 'https://th.bing.com/th/id/OIF.uyJ39PISgwNXYM0eN7IO9A?w=212&h=212&c=7&r=0&o=7&dpr=1.3&pid=1.7&rm=3', time: '10 min', postImage: 'https://th.bing.com/th/id/OIF.jF3cBEGCDSphdEbnIeX5YA?w=314&h=180&c=7&r=0&o=7&dpr=1.3&pid=1.7&rm=3', caption: 'Its me', reacts: 12, commentCount: 4 ),
-    PostModel(name: 'Lucas Stephan', profilePicture: 'https://th.bing.com/th/id/OIP.LenuNeUTPfv5N1rDjVWwuQHaLH?w=130&h=195&c=7&r=0&o=7&dpr=1.3&pid=1.7&rm=3', time: '18 min', postImage: 'https://th.bing.com/th/id/OIP.Nz39MBctbcL6_jhJ59wBpAHaHa?w=177&h=186&c=7&r=0&o=7&dpr=1.3&pid=1.7&rm=3', reacts: 123, commentCount: 44,caption: 'hello world' ),
-    PostModel(name: 'Sara Yuki', profilePicture: 'https://th.bing.com/th/id/OIP.H_Apy5HtxvhtWVS622dBMgHaLH?w=115&h=180&c=7&r=0&o=7&dpr=1.3&pid=1.7&rm=3', time: '2h', postImage: 'https://th.bing.com/th/id/OIP.DnQq__W5pibltm9H65jDLQHaE8?w=258&h=180&c=7&r=0&o=7&dpr=1.3&pid=1.7&rm=3', reacts: 244, commentCount: 112, caption: 'Just me!'),
+    PostModel(name: 'John Doe', profilePicture: 'https://th.bing.com/th/id/OIF.uyJ39PISgwNXYM0eN7IO9A?w=212&h=212&c=7&r=0&o=7&dpr=1.3&pid=1.7&rm=3', time: '10 minutes ago', postImage: 'https://th.bing.com/th/id/OIF.jF3cBEGCDSphdEbnIeX5YA?w=314&h=180&c=7&r=0&o=7&dpr=1.3&pid=1.7&rm=3', caption: 'Exploring the world', reacts: 12, commentCount: 4 ),
+    PostModel(name: 'Lucas Stephan', profilePicture: 'https://th.bing.com/th/id/OIP.LenuNeUTPfv5N1rDjVWwuQHaLH?w=130&h=195&c=7&r=0&o=7&dpr=1.3&pid=1.7&rm=3', time: '18 minutes ago', postImage: 'https://th.bing.com/th/id/OIP.Nz39MBctbcL6_jhJ59wBpAHaHa?w=177&h=186&c=7&r=0&o=7&dpr=1.3&pid=1.7&rm=3', reacts: 123, commentCount: 44,caption: 'Just be Cool bro !!' ),
+    PostModel(name: 'Sara Yuki', profilePicture: 'https://th.bing.com/th/id/OIP.H_Apy5HtxvhtWVS622dBMgHaLH?w=115&h=180&c=7&r=0&o=7&dpr=1.3&pid=1.7&rm=3', time: '1 hour ago', postImage: 'https://th.bing.com/th/id/OIP.DnQq__W5pibltm9H65jDLQHaE8?w=258&h=180&c=7&r=0&o=7&dpr=1.3&pid=1.7&rm=3', reacts: 244, commentCount: 112, caption: 'Embaracing the World'),
 
   ];
 
