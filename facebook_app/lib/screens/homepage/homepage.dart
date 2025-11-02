6f0e3dd1e56361f5ee3906cb2a88bf8ddef77430
Eranga <erangakavishanka@icloud.com>
2025-11-02 12:14:20 +0530

    extracted custom app bar also to a file

--- facebook_app/lib/screens/homepage/homepage.dart
+++ facebook_app/lib/screens/homepage/homepage.dart
@@ -2,6 +2,7 @@ import 'package:facebook_app/data/demo_data.dart';
 import 'package:facebook_app/screens/story_view_screen.dart';
 import 'package:flutter/material.dart';
 
+import 'widgets/custom_app_bar.dart';
 import 'widgets/custom_tab_bar.dart';
 import 'widgets/home_option_bar.dart';
 import 'widgets/post_list_view.dart';
@@ -65,54 +66,6 @@ class HomePage extends StatelessWidget {
 
 
 
-    );
-  }
-}
-
-
-
-
-
-
-class CustomAppBar extends StatelessWidget {
-  const CustomAppBar({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Row(children: [
-        Text('facebook', style: TextStyle(color: Colors.blue, fontSize: 30, fontWeight: FontWeight.bold )),
-        Spacer(),
-        IconButton(
-          onPressed: (){},
-           icon: Icon(Icons.add,),
-           style: ButtonStyle(
-            backgroundColor: WidgetStatePropertyAll(Colors.grey.shade300)
-           ),
-           
-           ),
-           IconButton(
-          onPressed: (){},
-           icon: Icon(Icons.search,),
-           style: ButtonStyle(
-            backgroundColor: WidgetStatePropertyAll(Colors.grey.shade300)
-           ),
-           
-           ),
-           IconButton(
-          onPressed: (){},
-           icon: Icon(Icons.messenger,),
-           style: ButtonStyle(
-            backgroundColor: WidgetStatePropertyAll(Colors.grey.shade300)
-           ),
-           
-           ),
-        
-              
-      ],),
     );
   }
 }
---
+++ facebook_app/lib/screens/homepage/widgets/custom_app_bar.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class CustomAppBar extends StatelessWidget {
+  const CustomAppBar({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Row(children: [
+        Text('facebook', style: TextStyle(color: Colors.blue, fontSize: 30, fontWeight: FontWeight.bold )),
+        Spacer(),
+        IconButton(
+          onPressed: (){},
+           icon: Icon(Icons.add,),
+           style: ButtonStyle(
+            backgroundColor: WidgetStatePropertyAll(Colors.grey.shade300)
+           ),
+           
+           ),
+           IconButton(
+          onPressed: (){},
+           icon: Icon(Icons.search,),
+           style: ButtonStyle(
+            backgroundColor: WidgetStatePropertyAll(Colors.grey.shade300)
+           ),
+           
+           ),
+           IconButton(
+          onPressed: (){},
+           icon: Icon(Icons.messenger,),
+           style: ButtonStyle(
+            backgroundColor: WidgetStatePropertyAll(Colors.grey.shade300)
+           ),
+           
+           ),
+        
+              
+      ],),
+    );
+  }
+}
