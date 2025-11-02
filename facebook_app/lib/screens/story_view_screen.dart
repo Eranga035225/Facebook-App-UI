565ac6c19112807d9011cb65d163c5f32fc02bb3
Eranga <erangakavishanka@icloud.com>
2025-11-02 09:26:20 +0530

    developed story view screen

--- facebook_app/lib/screens/story_view_screen.dart
+++ facebook_app/lib/screens/story_view_screen.dart
@@ -1,3 +1,4 @@
+import 'package:facebook_app/data/demo_data.dart';
 import 'package:flutter/material.dart';
 
 class StoryViewScreen extends StatefulWidget {
@@ -10,6 +11,42 @@ class StoryViewScreen extends StatefulWidget {
 class _StoryViewScreenState extends State<StoryViewScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor:Colors.black,
+      body: Stack(
+        children: [
+          Align(
+            alignment: Alignment.center,
+            child: Image.network(DemoData.stories[1].story,),),
+            Align(alignment: Alignment.bottomCenter,
+            child: SafeArea(child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                decoration: InputDecoration(
+                  fillColor: Colors.grey.shade900,
+                  filled: true,
+                  hintText: 'Type here ...',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(50),
+
+                    
+
+                  )
+
+                ),
+
+
+
+
+              ),
+            )),
+            )
+
+        ],
+
+
+
+      )
+    );
   }
 }
