2d85636c2875b0b9b1f2e34a6855e1db025360f4
Eranga <erangakavishanka@icloud.com>
2025-11-02 09:25:44 +0530

    changed demo data dart page

--- facebook_app/lib/data/demo_data.dart
+++ facebook_app/lib/data/demo_data.dart
@@ -10,14 +10,14 @@ class DemoData {
   
 
     StoryModel(profilePicture: 'https://th.bing.com/th/id/OIP.GjQkNLBt1b2rykCqh4paZwHaLI?w=122&h=184&c=7&r=0&o=7&dpr=1.3&pid=1.7&rm=3',
-    story:  'https://th.bing.com/th/id/OIF.loeLi75eugN2z2EEKgiSOg?w=225&h=180&c=7&r=0&o=7&dpr=1.3&pid=1.7&rm=3'
+    story:  'https://tse2.mm.bing.net/th/id/OIF.idtfdp4PhY5F3hiUfDkSDg?rs=1&pid=ImgDetMain&o=7&rm=3'
     ),
       StoryModel(profilePicture: 'https://th.bing.com/th/id/OIP.DnQq__W5pibltm9H65jDLQHaE8?w=301&h=201&c=7&r=0&o=7&dpr=1.3&pid=1.7&rm=3',
-    story:  'https://th.bing.com/th/id/OIF.RyLQDxxq2TiyvoH3IK3A1w?w=229&h=180&c=7&r=0&o=7&dpr=1.3&pid=1.7&rm=3'
+    story:  'https://tse4.mm.bing.net/th/id/OIP.BebOSO8kVsOm7f2BfAnb4gHaJO?rs=1&pid=ImgDetMain&o=7&rm=3'
     ),
 
     StoryModel(profilePicture:'https://th.bing.com/th/id/OIP.kfM6gE5n_IxPxrdgZg7SigHaLH?w=116&h=183&c=7&r=0&o=7&dpr=1.3&pid=1.7&rm=3' ,
-     story: 'https://th.bing.com/th?q=Attitude+Boy+Facebook+Profile&w=120&h=120&c=1&rs=1&qlt=70&o=7&cb=1&dpr=1.3&pid=InlineBlock&rm=3&mkt=en-WW&cc=LK&setlang=en&adlt=moderate&t=1&mw=247')
+     story: 'https://tse4.mm.bing.net/th/id/OIP.1BLgvKRl81t03VsPZpifdgHaNN?rs=1&pid=ImgDetMain&o=7&rm=3')
 
 
 
