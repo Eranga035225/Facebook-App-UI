63fb1b080c5b4895431b532a50f2decd546f5de6
Eranga <erangakavishanka@icloud.com>
2025-11-02 10:47:44 +0530

    completed navigations in story view screen

--- facebook_app/lib/screens/story_view_screen.dart
+++ facebook_app/lib/screens/story_view_screen.dart
@@ -18,6 +18,7 @@ class _StoryViewScreenState extends State<StoryViewScreen> {
 
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.sizeOf(context);
     return Scaffold(
       backgroundColor:Colors.black,
       body: Stack(
@@ -29,7 +30,44 @@ class _StoryViewScreenState extends State<StoryViewScreen> {
               onTap: (){
                 Navigator.pop(context);
               },
-              child: Image.network(widget.story.story, width:MediaQuery.sizeOf(context).width, fit: BoxFit.cover)),),
+              child: Image.network(widget.story.story, width:size.width, fit: BoxFit.cover)
+              ),
+              
+              
+              ),
+          SizedBox(width:size.width, height: size.height,
+          child: Row(children: [
+            GestureDetector(
+              onTap: (){
+                Navigator.pop(context);
+              },
+              child: Container( //left
+                height: size.height,
+                width: size.width/2,
+                color: Colors.transparent
+              ),
+            ),
+            GestureDetector(
+              onTap: (){
+                // print('Right side clicked');
+
+              },
+              child: Container( //right
+                height: size.height,
+                width: size.width/2,
+                color: Colors.transparent
+              ),
+            )
+
+         
+
+
+            ]
+          )
+          ,),
+          
+          
+         
           Align(alignment: Alignment.bottomCenter,
             child: SafeArea(child: Padding(
               padding: const EdgeInsets.all(8.0),
